8392811a7cf41404afe0d4a1ed1c577f276cea67
hrallapalli <hari.rallapalli@gmail.com>
2017-01-19 16:36:19 -0500

    Is this thing working

---
+++ learning_FunWithStrings.py
@@ -0,0 +1,7 @@
+a = "my first test string"
+b = 'another test string that I have defined'
+c = "this is Hari's string"
+d = "My favorite word is 'asparagus', what is yours?"
+
+math_string = '3+4*2'
+expression_string = " a+ ' ' +b+ ' tiger'"
