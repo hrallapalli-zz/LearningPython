bcf363029906cfc8c8dc4a7ff205f3e9fac98257
hrallapalli <hari.rallapalli@gmail.com>
2017-01-20 14:12:00 -0500

    Added FactorialProgram2

--- learning_FactorialProgram.py
+++ learning_FactorialProgram.py
@@ -1,9 +1,9 @@
-number = input("Enter a non-negative integer to take a factorial of")
+number = input("Enter a non-negative integer to take a factorial of: ")
 
 product = 1
 
 for i in range(number):
 
-    product = product*(i+1)
+    product = product * (i+1)
 
 print(product)
---
+++ learning_FactorialProgram2.py
@@ -0,0 +1,19 @@
+# Factorial program: learning_FactorialProgram2.copyright
+
+# Returns the factorial of the argument "number"
+def factorial(number):
+
+    product = 1
+
+    for i in range(number):
+
+        product = product * (i+1)
+
+    return product
+
+
+
+userInput = input("Enter a non-negative integer to take the factorial of: ")
+
+factorial_userInput = factorial(userInput)
+print(factorial_userInput)
